35328aa39138cc69f91ac91f6d2c664d9870f1c9
jgojiz <jhormangj@gmail.com>
2021-10-07 21:06:57 -0500

    intento de conectar a DB

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,11 +1,12 @@
 // Con esta API se definen las tareas que el servidor realiza sobre los datos
-
+// crea y almacena las rutas
 const express = require('express');
-const router = express.Router(); // crea y almacena las rutas
+const router = express.Router(); 
 
-// almacenar las rutas
+// definir las rutas
 router.get('/', (req, res) => {
     res.send('API Tasks')
 });
 
+// exportar rutas
 module.exports = router; // exporto el router para usarlo en otras partes de mi app
--- src/server.js
+++ src/server.js
@@ -2,8 +2,25 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
 
 const app = express();
+mongoose.connect('mongodb://localhost/sample_restaurants')
+    .then(db => console.log('DB connected'))
+    .catch(err => console.error(err));
+
+const uri = "mongodb+srv://rootMinTICG70-4:dJ0W7sLmtcqKsjZq@cluster0.1e51s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const collection = client.db("sample_restaurants").collection("neighborhoods");
+    // perform actions on the collection object
+    collection.find().toArray((err, name) => {
+        console.log(name)
+    })
+    client.close();
+});
+
+
 // settings
 app.set('port', process.env.PORT || 3000);
 
@@ -11,7 +28,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(morgan('dev')); // quiero ver los tiempos de ejecucion del comando dev definido en package.json
 app.use(express.json()); // con esto el server puede entender los archivos json que reciba
 
-// routes -> urls del server
+// routes -> urls para las acciones del server
 app.use('/tasks', require('./routes/tasks'));
 
 // static files -> server send them once to the client
